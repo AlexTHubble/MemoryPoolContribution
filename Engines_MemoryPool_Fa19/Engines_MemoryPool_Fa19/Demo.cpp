53df9ea16d2ae166ed119391b7b847e7f3d231dc
AlexHubble - Laptop <alexanderhubble885@gmail.com>
2019-12-03 09:51:53 -0500

    Added second test

--- Engines_MemoryPool_Fa19/Engines_MemoryPool_Fa19/Demo.cpp
+++ Engines_MemoryPool_Fa19/Engines_MemoryPool_Fa19/Demo.cpp
@@ -22,6 +22,27 @@ int main()
 	int* testData3 = pool->getEntry(dataID3);
 	*testData3 = 3;
 
+	std::cout << "Test 1" << std::endl;
+	std::cout << *testData << std::endl;
+	std::cout << *testData2 << std::endl;
+	std::cout << *testData3 << std::endl;
+
+	pool->removeEntry(dataID);
+	dataID = pool->setEntry();
+	testData = pool->getEntry(dataID);
+	*testData = 55555;
+
+	pool->removeEntry(dataID2);
+	dataID2 = pool->setEntry();
+	testData2 = pool->getEntry(dataID2);
+	*testData2 = 5;
+
+	pool->removeEntry(dataID3);
+	dataID3 = pool->setEntry();
+	testData3 = pool->getEntry(dataID3);
+	*testData3 = 6;
+
+	std::cout << "Test 2" << std::endl;
 	std::cout << *testData << std::endl;
 	std::cout << *testData2 << std::endl;
 	std::cout << *testData3 << std::endl;
